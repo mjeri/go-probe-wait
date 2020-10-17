21c33914a177d132911d549482f7dc939076fa93
Marco Jantke <marco.jantke@sonomotors.com>
2020-10-17 10:01:46 +0200

    add util script to build releases

---
+++ build-releases.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+readonly OS_LIST=("darwin" "linux" "windows")
+readonly ARCH_LIST=("amd64")
+
+if [[ ! -d "./releases" ]]; then
+  mkdir releases
+fi
+
+for os in "${OS_LIST[@]}"; do
+  for arch in "${ARCH_LIST[@]}"; do
+    GOOS="${os}" GOARCH="${arch}" go build -o "releases/go-wait-probe-${os}-${arch}"
+  done
+done
+
